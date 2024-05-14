83547b73dcf004efe6505d7004a375efde8fb591
Akash Rana <akashgujjar273@gmail.com>
2024-05-14 11:04:58 +0530

    handled condition with sum of 2 number

--- assignment.test.js
+++ assignment.test.js
@@ -8,6 +8,9 @@ test('add function returns the sum of one number', () => {
   expect(add('1')).toBe(1);
 });
 
+test('add function returns the sum of two numbers', () => {
+  expect(add('1,2')).toBe(3);
+});
 
 test('add function throws an exception for negative numbers', () => {
   expect(() => {
