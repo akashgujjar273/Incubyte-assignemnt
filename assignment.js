bde771f46cbc6a5d4173bc696cb0a3672122d456
Akash Rana <akashgujjar273@gmail.com>
2024-05-14 10:52:47 +0530

    handled condition with negative values amd splitted input values based on comma or delimiter

--- assignment.js
+++ assignment.js
@@ -1,9 +1,16 @@
 function add(params) {
-    
+    let commaSepratorString = ',';
     let nums = params;
 
     if (nums === '') {
         return 0;
+    }
+
+    const numArray = nums.split(new RegExp(`[${commaSepratorString}\n]`));
+    const negatives = numArray.filter(num => parseInt(num, 10) < 0);
+
+    if (negatives.length > 0) {
+        throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
     }
 
 }
